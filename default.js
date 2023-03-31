ee0ab82fd4d8e69f978c5892d55da3e23d32d77a
arcyia <arcyia.r@gmail.com>
2023-03-31 19:41:56 +0800

    Update default.js

--- default.js
+++ default.js
@@ -1,26 +1,42 @@
-export default function lbkMotion() {
-    // transition
-    const transition = {
-        thunder: {
-            ease: [0, 0.8, 0.4, 1.3],
-            duration: 0.3,
-        },
-    }
+// transition
+export const lbkTransition = {
+    lbkSpring: {
+        ease: [0, 0.8, 0.4, 1.3],
+        duration: 0.3,
+    },
+    lbkEase: { ease: [0, 0.8, 0.4, 1], duration: 0.3 },
+    lbkExit: {
+        ease: [0, 0.8, 0.4, 1],
+        duration: 0.2,
+    },
+}
 
-    // Animation
-    const animation = {
-        popInitial: {
-            opacity: 0,
-            y: 6,
-            rotateX: 6,
-        },
-        popAppear: {
-            opacity: 1,
-            y: 0,
-            rotateX: 0,
-            transition: transition.thunder,
-        },
-    }
+// Animation
+export const lbkAnimation = {
+    // Item
+    itemInitial: {
+        opacity: 0,
+        y: 8,
+        transition: lbkTransition.lbkExit,
+    },
+    itemAppear: {
+        opacity: 1,
+        y: 0,
+        delay: i * 0.1,
+        transition: lbkTransition.lbkEase,
+    },
 
-    return { transition, animation }
+    // Popover
+    popInitial: {
+        opacity: 0,
+        y: -8,
+        rotateX: -6,
+        transition: lbkTransition.lbkExit,
+    },
+    popAppear: {
+        opacity: 1,
+        y: 0,
+        rotateX: 0,
+        transition: lbkTransition.lbkSpring,
+    },
 }
